78f0bf1fff432341f49246d043bd6b974c8c4ceb
Ozi <okaanyildiz@gmail.com>
2023-01-07 12:32:59 +0100

    Create SearchBookPage and SearchBook component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
+import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
 
 function App() {
   return (
     <>
       <Navbar />
       <HomePage />
+      <SearchBooksPage />
       <Footer />
     </>
   );
