f1cd81a45eac602876d609f75281edc7b8c052c7
Ozi <okaanyildiz@gmail.com>
2023-01-07 14:29:32 +0100

    Create backend Search functionality

--- backend/library/src/main/java/ozi/library/dao/BookRepository.java
+++ backend/library/src/main/java/ozi/library/dao/BookRepository.java
@@ -1,9 +1,12 @@
 package ozi.library.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ozi.library.entity.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
