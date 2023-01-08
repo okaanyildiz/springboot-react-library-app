0ac0738521b84c9f6ba9faaa9616bcb5d3deb65d
Ozi <okaanyildiz@gmail.com>
2023-01-08 15:52:54 +0100

    Create ReviewRepository and find review by id functionality

--- backend/library/src/main/java/ozi/library/config/MyDataRestConfig.java
+++ backend/library/src/main/java/ozi/library/config/MyDataRestConfig.java
@@ -7,36 +7,40 @@ import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import ozi.library.entity.Book;
+import ozi.library.entity.Review;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
-    private String theAllowedOrigins = "http://localhost:3000";
+        private String theAllowedOrigins = "http://localhost:3000";
 
-    @Override
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
-            CorsRegistry cors) {
-        HttpMethod[] theUnsupportedActions = {
-                HttpMethod.POST,
-                HttpMethod.PATCH,
-                HttpMethod.DELETE,
-                HttpMethod.PUT };
+        @Override
+        public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
+                        CorsRegistry cors) {
+                HttpMethod[] theUnsupportedActions = {
+                                HttpMethod.POST,
+                                HttpMethod.PATCH,
+                                HttpMethod.DELETE,
+                                HttpMethod.PUT };
 
-        config.exposeIdsFor(Book.class);
+                config.exposeIdsFor(Book.class);
+                config.exposeIdsFor(Review.class);
 
-        disableHttpMethods(Book.class, config, theUnsupportedActions);
+                disableHttpMethods(Book.class, config, theUnsupportedActions);
+                disableHttpMethods(Review.class, config, theUnsupportedActions);
 
-        /* Configure CORS Mapping */
-        cors.addMapping(config.getBasePath() + "/**")
-                .allowedOrigins(theAllowedOrigins);
-    }
+                /* Configure CORS Mapping */
+                cors.addMapping(config.getBasePath() + "/**")
+                                .allowedOrigins(theAllowedOrigins);
+        }
 
-    private void disableHttpMethods(Class theClass,
-            RepositoryRestConfiguration config,
-            HttpMethod[] theUnsupportedActions) {
-        config.getExposureConfiguration()
-                .forDomainType(theClass)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
-    }
+        private void disableHttpMethods(Class theClass,
+                        RepositoryRestConfiguration config,
+                        HttpMethod[] theUnsupportedActions) {
+                config.getExposureConfiguration()
+                                .forDomainType(theClass)
+                                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                                .withCollectionExposure(
+                                                (metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+        }
 }
