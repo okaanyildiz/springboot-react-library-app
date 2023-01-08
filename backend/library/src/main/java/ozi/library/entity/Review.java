ecd9719715fb055a46fbdd805e6899320fc82dc7
Ozi <okaanyildiz@gmail.com>
2023-01-08 15:10:08 +0100

    Create Review entity

---
+++ backend/library/src/main/java/ozi/library/entity/Review.java
@@ -0,0 +1,33 @@
+package ozi.library.entity;
+
+import java.util.Date;
+import org.hibernate.annotations.CreationTimestamp;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "review")
+@Data
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "user_name")
+    private String userEmail;
+
+    @Column(name = "date")
+    @CreationTimestamp
+    private Date date;
+
+    @Column(name = "rating")
+    private double rating;
+
+    @Column(name = "book_id")
+    private Long bookId;
+
+    @Column(name = "review_description")
+    private String revieDescription;
+}
